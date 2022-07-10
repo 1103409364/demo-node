d07f496dfb78fabd9b24376b387723e368f769ea
1103409364 <1103409364@qq.com>
2022-07-10 15:04:07 +0800

    chore: vscode debug 配置
    Signed-off-by: 1103409364 <1103409364@qq.com>

--- src/devServer.js
+++ src/devServer.js
@@ -1,6 +1,6 @@
 // 通过快捷键，在 vscode 中打开页面 html。参考 https://github.com/zhouwei1994/code-link-plugin
 // 前端 get 请求当前服务发送文件路径
-const net = require("net")
+const net = require("net");
 const http = require("http");
 const urlParser = require("url");
 const path = require("path");
@@ -8,7 +8,7 @@ const child_process = require("child_process");
 
 function startDevServer(port) {
   const hostname = "localhost";
-  
+
   const server = http.createServer((req, res) => {
     const { url } = req;
     setHeader(res);
@@ -23,7 +23,7 @@ function startDevServer(port) {
     res.statusCode = 200;
     res.end("success");
   });
-  
+
   function setHeader(res) {
     res.setHeader("Content-Type", "text/plain;charset=utf8");
     res.setHeader("Access-Control-Allow-Origin", "*");
@@ -31,7 +31,7 @@ function startDevServer(port) {
     res.setHeader("Access-Control-Allow-Headers", "customer-header, customer-header2");
     res.setHeader("Access-Control-Allow-Methods", "POST, PUT");
   }
-  
+
   server.listen(port, () => {
     console.log(`devServer running on http://${hostname}:${port}`);
   });
@@ -41,7 +41,7 @@ function findUnusedPort(port) {
   const server = net.createServer().listen(port);
   return new Promise((resolve, reject) => {
     server.on("listening", () => {
-      console.log(`the server is runnint on port ${port}`);
+      console.log(`the server is running on port ${port}`);
       server.close();
       resolve(port);
     });
@@ -60,7 +60,7 @@ function findUnusedPort(port) {
 async function main() {
   try {
     const port = await findUnusedPort(3000);
-    startDevServer(port)
+    startDevServer(port);
   } catch (err) {
     console.error(err);
   }
---
+++ src/test.ts
@@ -0,0 +1,3 @@
+let a: any = 1;
+a += `${1}单独1`;
+console.log(a);
