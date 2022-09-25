60ddb885e07c15b07648c0bd47632d69137dec04
wcs <1103409364@qq.com>
2022-09-25 10:44:01 +0800

    chore: 调整目录结构
    Signed-off-by: wcs <1103409364@qq.com>

--- src/shell/restart.sh
+++ src/shell/restart.sh
@@ -4,13 +4,13 @@ echo "备份 $name"
 rename $name $name-$(date "+%Y%m%d-%H%M%S").jar $name
 
 echo "  ===== 关闭 xxx ======"
-PROCESS=$(ps -ef | grep java | grep -v grep | grep /mnt/clear/java/clear/config/application.yaml | awk '{print $2}')
+PROCESS=$(ps -ef | grep java | grep -v grep | grep /var/java/clear/config/application.yaml | awk '{print $2}')
 for i in $PROCESS; do
   echo "Kill the $1 process [ $i ]"
   kill -9 $i
 done
 echo "  ===== 启动 xxx ======"
-nohup java -jar -Dspring.config.location="/xxx/java/clear/config/application.yaml" -Xms1024m -Xmx1024m $name >log.out 2>&1 &
+nohup java -jar -Dspring.config.location="/var/java/clear/config/application.yaml" -Xms1024m -Xmx1024m $name >log.out 2>&1 &
 echo $! >savePid.txt
 cat savePid.txt
 rm savePid.txt
