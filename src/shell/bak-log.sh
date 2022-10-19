5dae29e3d192fdc6081df6908d35594f5d4cb3ab
1103409364 <1103409364@qq.com>
2022-10-19 13:38:56 +0800

    日志备份
    Signed-off-by: 1103409364 <1103409364@qq.com>

---
+++ src/shell/bak-log.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# $() 取命令结果
+file=/tmp/log-$(date "+%Y%m%d").tar.gz
+
+# 把备份文件放到 tmp。-f 判断文件存在，! 取反
+if [ ! -f $file ]; then # [ ] 和命令之间要保留空格，否则报错
+  tar -czf $file /var/log/
+fi
+# 把脚本放到定时任务，每天执行备份
