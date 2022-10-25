9e4e3767edced08d685e6a9a50fe3ae5c1c2086a
1103409364 <1103409364@qq.com>
2022-10-25 14:10:39 +0800

    增量代码文件导出，命令行参数加校验
    Signed-off-by: 1103409364 <1103409364@qq.com>

--- src/shell/git-diff-archive.sh
+++ src/shell/git-diff-archive.sh
@@ -1,16 +1,36 @@
 #!/bin/bash
-
-echo -e "#\e[32m 导出 git 增量代码，参数1：bug 编号，参数2：commit hash\e[0m           #" # \e 同 \033
+# 增量代码文件导出
+echo -e "#\e[32m 导出 git 增量代码\e[0m" # \e 同 \033
 echo
 
 read -p "请输入项目路径，使用/或者\\\作为路径分隔符，当前路径直接回车：" path
-
+# 路径非空
 if [ ! -z $path ]; then
   cd $path
 fi
 
-echo "当前路径："$(pwd)
+if [ $? -ne 0 ]; then
+  echo -e "#\e[35m 路径 $path 不存在，请检查 \e[0m"
+  exit
+fi
 
-read -p "请输入 bug 编号：" bugNo
-read -p "请输入 Commit Hash：" commitHash
-git archive -o ./$bugNo.zip $commitHash $(git diff --name-only "$commitHash^!")
+echo -e "#\e[35m 当前路径：$(pwd)\e[0m"
+
+while [ -z $bugNo ]; do
+  read -p "请输入 bug 编号：" bugNo
+done
+
+while [ -z $commitHash ]; do
+  read -p "请输入 Commit Hash：" commitHash
+done
+
+# 输出路径
+file=$(pwd)/$bugNo.zip
+git archive -o $file $commitHash $(git diff --name-only "$commitHash^!")
+
+if [ $? -eq 0 ]; then
+  echo -e "#\e[34m 导出成功：$file \e[0m"
+else
+  echo -e "#\e[35m 导出失败，请检查参数 \e[0m"
+  rm $file
+fi
