58fd7d04586a3020dc25c6f0c2b353396e9f424b
1103409364 <1103409364@qq.com>
2023-01-06 10:05:49 +0800

    fix: df awk 正则
    Signed-off-by: 1103409364 <1103409364@qq.com>

--- src/shell/awk-menu.sh
+++ src/shell/awk-menu.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo -e "\033[42m-------------------------\033[0m"
-echo -e "#\e[32m 1.查看剩余内存\e[0m           #" # \e 同 \033
-echo -e "#\e[33m 2.查看根分区剩余容量\e[0m           #"
-echo -e "#\e[34m 3.查看CPU十五分钟负载\e[0m           #"
+echo -e "#\e[32m 1.查看剩余内存（MB）\e[0m           #" # \e 同 \033
+echo -e "#\e[33m 2.查看根分区剩余容量（MB）\e[0m           #"
+echo -e "#\e[34m 3.查看 CPU 十五分钟负载\e[0m           #"
 echo -e "#\e[35m 4.查看系统进程数量\e[0m           #"
 echo -e "#\e[36m 5.查看系统账户数量\e[0m           #"
 echo -e "#\e[36m 6.退出\e[0m           #"
@@ -14,10 +14,10 @@ while :; do
   read -p "请选择[1-6]：" i
   case $i in
   1)
-    free | awk '/Mem/{print $NF}'
+    free -m | awk '/Mem/{print $NF " MB"}'
     ;; # case 命令序列最后必须以双分号结尾
   2)
-    df | awk '/\/${print $4}' # 根分区 斜线结尾的行
+    df -m | awk '/\/$/{print $4 " MB"}' # 根分区 斜线结尾的行
     ;;
   3)
     uptime | awk '{print $NF}'
