2a4881984b5b523d911ec32db4d810fccd8f861f
1103409364 <1103409364@qq.com>
2023-01-06 10:14:57 +0800

    feat: 新增命令
    Signed-off-by: 1103409364 <1103409364@qq.com>

--- src/shell/awk-menu.sh
+++ src/shell/awk-menu.sh
@@ -5,13 +5,14 @@ echo -e "#\e[32m 1.查看剩余内存（MB）\e[0m           #" # \e 同 \033
 echo -e "#\e[33m 2.查看根分区剩余容量（MB）\e[0m           #"
 echo -e "#\e[34m 3.查看 CPU 十五分钟负载\e[0m           #"
 echo -e "#\e[35m 4.查看系统进程数量\e[0m           #"
-echo -e "#\e[36m 5.查看系统账户数量\e[0m           #"
-echo -e "#\e[36m 6.退出\e[0m           #"
+echo -e "#\e[36m 5.查看 JAVA 进程\e[0m           #"
+echo -e "#\e[36m 6.查看系统账户数量\e[0m           #"
+echo -e "#\e[36m 7.退出\e[0m           #"
 echo -e "\033[42m-------------------------\033[0m"
 echo
 
 while :; do
-  read -p "请选择[1-6]：" i
+  read -p "请选择 [1-7]：" i
   case $i in
   1)
     free -m | awk '/Mem/{print $NF " MB"}'
@@ -26,9 +27,12 @@ while :; do
     ps aux | wc -l # wc 统计
     ;;
   5)
-    sed -n '$=' /etc/passwd # 最后一行行号。$ 最后一行，= 打印
+    ps aux | grep java
     ;;
   6)
+    sed -n '$=' /etc/passwd # 最后一行行号。$ 最后一行，= 打印
+    ;;
+  7)
     exit
     ;;
   *)
