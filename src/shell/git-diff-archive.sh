f6e302cd26166030022904b793f62af8ffbec73b
1103409364 <1103409364@qq.com>
2023-02-03 11:48:40 +0800

    fix: read -r
    Signed-off-by: 1103409364 <1103409364@qq.com>

--- src/shell/git-diff-archive.sh
+++ src/shell/git-diff-archive.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
+
 # 增量代码文件导出
 echo -e "#\e[32m 导出 git 增量代码\e[0m" # \e 同 \033
 echo
 
-read -p "请输入项目路径，使用/或者\\\作为路径分隔符，当前路径直接回车：" path
+read -r -p "请输入项目路径，使用/或者\\\作为路径分隔符，当前路径直接回车：" path
 # 路径非空
-if [ ! -z $path ]; then
-  cd $path
+if [ ! -z "$path" ]; then
+  cd "$path" || exit
 fi
 
 if [ $? -ne 0 ]; then
@@ -16,21 +17,21 @@ fi
 
 echo -e "#\e[35m 当前路径：$(pwd)\e[0m"
 
-while [ -z $bugNo ]; do
-  read -p "请输入 bug 编号：" bugNo
+while [ -z "$bugNo" ]; do
+  read -r -p "请输入 bug 编号：" bugNo
 done
 
-while [ -z $commitHash ]; do
-  read -p "请输入 Commit Hash：" commitHash
+while [ -z "$commitHash" ]; do
+  read -r -p "请输入 Commit Hash：" commitHash
 done
 
 # 输出路径
 file=$(pwd)/$bugNo.zip
-git archive -o $file $commitHash $(git diff --name-only "$commitHash^!")
+git archive -o "$file" "$commitHash" "$(git diff --name-only "$commitHash^!")"
 
 if [ $? -eq 0 ]; then
   echo -e "#\e[34m 导出成功：$file \e[0m"
 else
   echo -e "#\e[35m 导出失败，请检查参数 \e[0m"
-  rm $file
+  rm "$file"
 fi
