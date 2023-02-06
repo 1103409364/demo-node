fa7b042b794408488ddd4dc60027154c52c928f4
1103409364 <1103409364@qq.com>
2023-02-06 11:19:46 +0800

    fix: Useless cat
    Signed-off-by: 1103409364 <1103409364@qq.com>

--- src/shell/sed-ibook.sh
+++ src/shell/sed-ibook.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # Useless cat. Consider 'cmd < file | ..' or 'cmd file | ..' instead.shellcheckSC2002
-cat test.txt | sed -E -e 's/^“//g' | sed -E -e 's/”$//g' | sed -E -e '/^(摘录来自)/,$d'
+# s/查找的内容/替换的内容/ 这里空表示删除。分隔符号也可以用 # 代替
+sed -E -e 's#^“##g' test.txt | sed -E -e 's/”$//g' | sed -E -e '/^(摘录来自)/,$d'
